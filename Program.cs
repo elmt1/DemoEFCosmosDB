d3cc59ac84ce8ef491b3ad321eb2a203258cb7cf
David Morris <David.Morris@weyerhaeuser.com>
2019-11-16 14:10:50 -0800

    Cleanup so that classes aren't nested

--- Program.cs
+++ Program.cs
@@ -8,37 +8,6 @@ namespace DemoEFCosmos
 {
     class Program
     {
-        public class DemoContext : DbContext
-        {
-            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-                => optionsBuilder.UseCosmos(
-                    "https://localhost:8081",
-                    "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
-                    databaseName: "Locations");
-
-            public DbSet<Parent>? Parent { get; set; }
-
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                modelBuilder.HasDefaultContainer("Demo");
-                modelBuilder.Entity<Parent>().OwnsOne(b => b.Singleton);
-                modelBuilder.Entity<Parent>().OwnsMany(b => b.Children);
-            }
-        }
-        public class Parent
-        {
-            public Guid Id { get; set; }
-            public Singleton? Singleton { get; set; }
-            public ICollection<Child>? Children { get; set; }
-        }
-        public class Singleton
-        {
-            public string SingletonName { get; set; } = string.Empty;
-        }
-        public class Child
-        {
-            public string ChildName { get; set; } = string.Empty;
-        }
         static void Main(string[] args)
         {
             using (var context = new DemoContext())
@@ -58,6 +27,7 @@ namespace DemoEFCosmos
                     Children = children
                 };
 
+                context.Parent?.Add(addParent);
                 context.SaveChanges();
 
                 Debug.WriteLine("Count Before: " + addParent.Children?.Count);
@@ -74,5 +44,36 @@ namespace DemoEFCosmos
                 Debug.WriteLine("Count After: " + children.Count);
             }
         }
+    }
+    public class DemoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseCosmos(
+                "https://localhost:8081",
+                "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+                databaseName: "Locations");
+
+        public DbSet<Parent>? Parent { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultContainer("Demo");
+            modelBuilder.Entity<Parent>().OwnsOne(b => b.Singleton);
+            modelBuilder.Entity<Parent>().OwnsMany(b => b.Children);
+        }
+    }
+    public class Parent
+    {
+        public Guid Id { get; set; }
+        public Singleton? Singleton { get; set; }
+        public ICollection<Child>? Children { get; set; }
+    }
+    public class Singleton
+    {
+        public string SingletonName { get; set; } = string.Empty;
+    }
+    public class Child
+    {
+        public string ChildName { get; set; } = string.Empty;
     }
 }
